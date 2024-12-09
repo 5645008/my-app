1b9ab04adb288dbae0287f26daaf04f7ab3a3682
C7266 <cubic1102@naver.com>
2024-12-09 13:30:18 +0900

    텍스트 스캔 화면 수정

--- src/pages/TextScanPage.js
+++ src/pages/TextScanPage.js
@@ -1,23 +1,27 @@
-// TextScanPage.js
-import React  from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import Camera  from './TextScan/Camera';
-import TextScanner  from './TextScan/TextScanner';
-import ImagePreview  from './TextScan/ImagePreview';
-import useCamera  from './TextScan/useCamera';
+import Camera from './TextScan/Camera';
+import TextScanner from './TextScan/TextScanner';
+import ImagePreview from './TextScan/ImagePreview';
+import useCamera from './TextScan/useCamera';
 import back from '../assets/back_arrow.png';
-import camera from '../assets/camera.png';
+import cameraIcon from '../assets/camera.png';
 import '../css/TextScanPage.styled.css';
 
 function TextScanPage() {
   const { image, captureImage } = useCamera();
-  console.log({ image, captureImage });
 
   return (
     <div className="TextScanPage">
-      <Link to="/main"><img src={back} width="20px" alt="back" /></Link>
-      <h1><img src={camera} alt="Camera" className="button-icon" /></h1>
-      <Camera onCapture={captureImage} />
+      <Link to="/main">
+        <img src={back} width="20px" alt="뒤로 가기" />
+      </Link>
+      <h1>
+        <img src={cameraIcon} alt="Camera" className="button-icon" />
+      </h1>
+      <div className="camera-container">
+        <Camera onCapture={captureImage} />
+      </div>
       {image && (
         <>
           <ImagePreview image={image} />
